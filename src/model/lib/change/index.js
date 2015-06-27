018f9d73c2dbc07240035af6208ce1593c2952bf
naoya-ogawa <example@co.jp>
2015-06-28 01:15:01 +0900

    行サイズ変更処理追加

--- src/model/lib/change/index.js
+++ src/model/lib/change/index.js
@@ -2,29 +2,55 @@
 import {OBJECT_TYPE} from "../../gridview/object-type";
 
 // 列幅のリサイズ処理
-function columnResize(viewModel, opeModel){
+function columnResize(viewModel, opeModel) {
   const objectType = opeModel.opeItem && opeModel.opeItem.objectType;
-    if (objectType !== OBJECT_TYPE.COLUMN_RESIZER){
-      return viewModel;
-    }
+  if (objectType !== OBJECT_TYPE.COLUMN_RESIZER) {
+    return viewModel;
+  }
 
-    const selectItem = opeModel.selectItem;
-    if (!selectItem){
-      return viewModel;
-    }
+  const selectItem = opeModel.selectItem;
+  if (!selectItem) {
+    return viewModel;
+  }
 
-    // 新しい幅を計算酢る
-    const left = selectItem.rect.left;
-    const right = opeModel.hoverItem.point.x;
-    const width = (right <= left) ? 1 : right - left;
+  // 新しい幅を計算酢る
+  const left = selectItem.rect.left;
+  const right = opeModel.hoverItem.point.x;
+  const width = (right <= left) ? 1 : right - left;
 
-    // 列アイテムの更新
-    const columnNo = selectItem.target.columnNo;
-    const columnHeaderItem = viewModel.columnHeader.items.get(columnNo);
-    const newItem = columnHeaderItem.setWidth(width);
-    const newColumnHeader = viewModel.columnHeader.setItem(columnNo, newItem);
+  // 列アイテムの更新
+  const columnNo = selectItem.target.columnNo;
+  const columnHeaderItem = viewModel.columnHeader.items.get(columnNo);
+  const newItem = columnHeaderItem.setWidth(width);
+  const newColumnHeader = viewModel.columnHeader.setItem(columnNo, newItem);
 
-    return viewModel.setColumnHeader(newColumnHeader);
+  return viewModel.setColumnHeader(newColumnHeader);
+
+}
+
+function rowResize(viewModel, opeModel){
+  const objectType = opeModel.opeItem && opeModel.opeItem.objectType;
+  if (objectType !== OBJECT_TYPE.ROW_RESIZER) {
+    return viewModel;
+  }
+
+  const selectItem = opeModel.selectItem;
+  if (!selectItem) {
+    return viewModel;
+  }
+
+  // 新しい高さを算出する
+  const top = selectItem.rect.top;
+  const bottom = opeModel.hoverItem.point.y;
+  const height = (bottom <= top) ? 1 : bottom - top;
+
+  // 行アイテムの更新
+  const rowNo = selectItem.target.rowNo;
+  const rowHeaderItem = viewModel.rowHeader.items.get(rowNo);
+  const newItem = rowHeaderItem.setHeight(height);
+  const newRowHeader = viewModel.rowHeader.setItem(rowNo, newItem);
+
+  return viewModel.setRowHeader(newRowHeader);
 
 }
 
@@ -33,6 +59,12 @@ function operationResult(viewModel, opeModel){
 
   if (cResizeModel !== viewModel){
     return cResizeModel;
+  }
+
+  const rResizeModel = rowResize(viewModel, opeModel);
+
+  if (rResizeModel !== viewModel){
+    return rResizeModel;
   }
 
   return viewModel;
