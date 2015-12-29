420865889a5209001aa218e699a446535a81de38
ogaya <wadesu012@gmail.com>
2015-12-30 01:56:46 +0900

    update modules

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ var istanbul = require("gulp-istanbul");
 var mocha = require('gulp-mocha');
 //var gutil = require('gulp-util');
 var babel = require('gulp-babel');
-var espower = require('gulp-espower');
+//var espower = require('gulp-espower');
 
 gulp.task("cleanBuild", function (cb) {
     var rimraf = require("rimraf");
@@ -25,20 +25,13 @@ gulp.task("cleanBuild", function (cb) {
 //  .pipe(gulp.dest(""));
 //});
 
-//gulp.task("debug", ["cleanBuild"], function() {
-//  return gulp.src("")
-//  .pipe(webpack(webpackConfig))
-//  .pipe(gulp.dest(""));
-//});
-
-
 gulp.task("build", ["cleanBuild"], function () {
     return gulp.src('./src/**')
         .pipe(babel())
         .pipe(gulp.dest("./dist"));
 });
 
-gulp.task("sample", ["build", "test"], function () {
+gulp.task("sample", ["build"], function () {
     return gulp.src("")
         .pipe(webpack(webpackSampleConfig))
         .pipe(gulp.dest(""));
@@ -52,10 +45,6 @@ gulp.task("pre-test", function () {
 });
 
 gulp.task('test', ["pre-test"],function () {
-    //require('babel/register');
-    //require('intelli-espower-loader');
-
-    //return gulp.src(['./test/model/*.js'], { read: false })
     return gulp.src('./test/**/*.js')
         .pipe(mocha({}))
         .pipe(istanbul.writeReports());
