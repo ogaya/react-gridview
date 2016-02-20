6a9bd04cccf866fcf3da655616e2fd2f6b8000e3
ogaya <wadesu012@gmail.com>
2016-02-20 23:03:03 +0900

    refactoring

--- src/containers/cells/controls/column-header.ts
+++ src/containers/cells/controls/column-header.ts
@@ -5,7 +5,7 @@ import {RowHeader} from "../../../model/sheet/row-header";
 import {Operation} from "../../../model/operation";
 
 export default function drawColumnHeader(
-    canvas:Canvas, columnHeader:ColumnHeader, rowHeader:RowHeader, opeModel:Operation) {
+    canvas: Canvas, columnHeader: ColumnHeader, rowHeader: RowHeader, opeModel: Operation) {
 
     if (!columnHeader.isVisible) {
         return;
@@ -16,32 +16,32 @@ export default function drawColumnHeader(
     context.fillStyle = columnHeader.background;
     //左から20上から20の位置に幅50高さ50の塗りつぶしの四角形を描く
     context.fillRect(
-        rowHeader.width, 0, 
+        rowHeader.width, 0,
         opeModel.canvasRect.width, columnHeader.height);
     context.strokeStyle = "#999";
     context.lineWidth = 1;
-    
+
     canvas.drawLine(
-        rowHeader.width, columnHeader.height, 
+        rowHeader.width, columnHeader.height,
         opeModel.canvasRect.width, columnHeader.height);
-    
+
     const columnNo = opeModel.scroll.columnNo;
-    let sumWidth = rowHeader.width;
+    let sumWidth = (rowHeader.width) | 0;
     canvas.drawLine(sumWidth, 0, sumWidth, columnHeader.height);
-    
+
     let i = columnNo;
-    while(sumWidth < canvas.width){
+
+    canvas.context.fillStyle = columnHeader.color;
+    context.font = "11px arial";
+    while ((sumWidth | 0) < (canvas.width | 0)) {
         const item = columnHeader.items.get(i);
-        if (!item){
+        if (!item) {
             break;
         }
         const rect = new Rect(sumWidth, 0, item.width, columnHeader.height);
 
-        sumWidth = sumWidth + item.width;
+        sumWidth = (sumWidth + item.width) | 0;
         canvas.drawLine(sumWidth, 0, sumWidth, columnHeader.height);
-
-        canvas.context.fillStyle = columnHeader.color;
-        context.font = "11px arial";
         canvas.drawText(item.cell.value, rect, item.cell.textAlign, item.cell.verticalAlign);
         i = i + 1;
     }
