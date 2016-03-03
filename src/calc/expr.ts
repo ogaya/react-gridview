df14c427be05a76c58bc7331f2bae92ab8298f7c
ogaya <wadesu012@gmail.com>
2016-03-03 23:15:18 +0900

    upgrade typescript version

--- src/calc/expr.ts
+++ src/calc/expr.ts
@@ -4,58 +4,56 @@ import {term} from "./term";
 import {Solver} from "./solver";
 
 // <expr>   ::= <term> [ ('+'|'-') <term> ]*
-const expr = function(solver:Solver){
+const expr = function(solver: Solver) {
 
-  if (solver.isError){
-    return solver;
-  }
-
-  solver = term(solver);
-  while (true) {
-    let s = solver.pointSubstr(1);
-
-    let exprSolver = Solver.createEmpty()
-      .setView(solver.sheet)
-      .setRefIds(solver.refIds)
-      .setText(solver.addPointer().pointSubstr());
-    if(s === "+"){
-
-      solver = solver.addPointer();
-
-      exprSolver = term(exprSolver);
-      if((exprSolver.isError) || (isNaN(exprSolver.value))){
-        return solver.setIsError(true);
-      }
-
-      solver = solver
-        .setValue(solver.value + exprSolver.value)
-        .setRefIds(exprSolver.refIds)
-        .setIsError(solver.isError || exprSolver.isError)
-        .setPointer(solver.pointer + exprSolver.pointer);
-      continue;
+    if (solver.isError) {
+        return solver;
     }
-    else if(s === "-"){
-      solver = solver.addPointer();
 
-      exprSolver = term(exprSolver);
-      if((exprSolver.isError) || (isNaN(exprSolver.value))){
-        return solver.setIsError(true);
-      }
-      solver = solver
-        .setValue(solver.value - exprSolver.value)
-        .setRefIds(exprSolver.refIds)
-        .setIsError(solver.isError || exprSolver.isError)
-        .setPointer(solver.pointer + exprSolver.pointer);
-      continue;
-    }
-    else{
-      return solver;
-    }
-  }
+    solver = term(solver);
+    while (true) {
+        let s = solver.pointSubstr(1);
 
-  return solver;
+        let exprSolver = Solver.createEmpty()
+            .setView(solver.sheet)
+            .setRefIds(solver.refIds)
+            .setText(solver.addPointer().pointSubstr());
+        if (s === "+") {
+
+            solver = solver.addPointer();
+
+            exprSolver = term(exprSolver);
+            if ((exprSolver.isError) || (isNaN(exprSolver.value))) {
+                return solver.setIsError(true);
+            }
+
+            solver = solver
+                .setValue(solver.value + exprSolver.value)
+                .setRefIds(exprSolver.refIds)
+                .setIsError(solver.isError || exprSolver.isError)
+                .setPointer(solver.pointer + exprSolver.pointer);
+            continue;
+        }
+        else if (s === "-") {
+            solver = solver.addPointer();
+
+            exprSolver = term(exprSolver);
+            if ((exprSolver.isError) || (isNaN(exprSolver.value))) {
+                return solver.setIsError(true);
+            }
+            solver = solver
+                .setValue(solver.value - exprSolver.value)
+                .setRefIds(exprSolver.refIds)
+                .setIsError(solver.isError || exprSolver.isError)
+                .setPointer(solver.pointer + exprSolver.pointer);
+            continue;
+        }
+        else {
+            return solver;
+        }
+    }
 };
 
-export{
-  expr
+export {
+expr
 };
